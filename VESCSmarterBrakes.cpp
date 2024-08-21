7a7ed2941c46ff37a4e0f45a58498b82b6e44173
Chris Becker <cbecker333@gmail.com>
2024-08-21 16:58:10 -0700

    implement EEPROM storage of mode selection, reload it on startup. minor tweaks to timing, clarifications

--- VESCSmarterBrakes.cpp
+++ VESCSmarterBrakes.cpp
@@ -1,41 +1,42 @@
 #include "Arduino.h"
+#include <EEPROM.h>
 #include "VescUart.h"
 #include "VESCSmarterBrakes.h"
-
-
 
 VESCSmarterBrakes::VESCSmarterBrakes(int dimmerPin, int buttonPin) {
   pinMode(dimmerPin, OUTPUT);
   pinMode(buttonPin, INPUT_PULLUP);
   _dimmerPin = dimmerPin;
   _buttonPin = buttonPin;
-  SetDimmerPower(IDLE_POWER);
-  /** Setup UART port */
-  Serial.begin(DEFAULT_COMM_BAUD);
 
-  /** Define which ports to use as UART */
-  UART.setSerialPort(&Serial);
-  _mode = MODE_STROBE;
-  _lightOff = false;
+  _mode = MODE_STEADY;
   _idling = false;
   _brakeActive = false;
-  _buttonUnpressed = true;
   _startupSplashRate = 1;
   _startupSplashDelay = 2;
   _lastDebounceTime = 0;
-  _currentPower = MEDIUM_POWER;
+}
+
+void VESCSmarterBrakes::SetSerial(HardwareSerial* port) {
+  /** Define which ports to use as UART */
+  UART.setSerialPort(port);
 }
 
 void VESCSmarterBrakes::TurnOn() {
-  _lightOff = false;
-  
   // pulse animation on startup
   TransitionBrightness(HIGH_POWER, OFF);
   TransitionBrightness(OFF, MEDIUM_POWER);
   TransitionBrightness(MEDIUM_POWER, OFF);
   TransitionBrightness(OFF, MEDIUM_POWER);
   TransitionBrightness(MEDIUM_POWER, IDLE_POWER);
-  TransitionBrightness(IDLE_POWER, LOW_POWER);
+
+  // apply saved mode from EEPROM
+  int savedMode = EEPROM.read(MODE_EEPROM_ADDRESS);
+  if (savedMode != NULL && savedMode <= LAST_MODE) {
+    _mode = savedMode;
+    ApplyMode();
+  }
+  _lightOff = false;
 }
 
 void VESCSmarterBrakes::TransitionBrightness(int dStart, int dStop) {
@@ -63,11 +64,11 @@ void VESCSmarterBrakes::ReadMode() {
   if (sensorValue != _lastButtonValue) {
     _lastDebounceTime = millis();
   }
-  if (millis() - _lastDebounceTime > 50) {
+  if (millis() - _lastDebounceTime > 70) {
     if (sensorValue != _buttonValue) {
       _buttonValue = sensorValue;
       if (_buttonValue == LOW) {
-        ChangeMode();
+        CycleMode();
       }
     }
   }
@@ -75,33 +76,44 @@ void VESCSmarterBrakes::ReadMode() {
 }
 
 
-void VESCSmarterBrakes::ChangeMode() {
-  if (_mode == MODE_STROBE && !_brakeActive) {
-    SetDimmerPower(_currentPower);
-  }
+void VESCSmarterBrakes::CycleMode() {
   _mode++;
-  if (_mode > MODE_OFF) {
-    _mode = MODE_STROBE;
+  if (_mode > LAST_MODE) {
+    _mode = 0;
   }
-  if (_mode == MODE_LOW) {
-    _currentPower = LOW_POWER;
-  } else {
-    _currentPower = MEDIUM_POWER;
+  EEPROM.write(MODE_EEPROM_ADDRESS, _mode);
+  ApplyMode();
+}
+
+void VESCSmarterBrakes::ApplyMode() {
+  if (!_brakeActive) {
+    if (_mode == MODE_LOW) {
+      SetDimmerPower(LOW_POWER);
+
+    } else if (_mode == MODE_STROBE) {
+      SetDimmerPower(HIGH_POWER);
+
+    } else if (_mode == MODE_STEADY) {
+      SetDimmerPower(MEDIUM_POWER);
+
+    } else if (_mode == MODE_OFF) {
+      SetDimmerPower(OFF);
+    }
   }
 }
 
 void VESCSmarterBrakes::SetDimmerPower(int value) {
-  if (_currentPower == NULL || _currentPower != value) {
-    _currentPower = value;
+  if (_dimmerPower == NULL || _dimmerPower != value) {
+    _dimmerPower = value;
     analogWrite(_dimmerPin, value);
   }
 }
 
 void VESCSmarterBrakes::ApplyStrobe() {
-  if (_mode == MODE_STROBE && !_brakeActive) {
+  if (_mode == MODE_STROBE && !_brakeActive && !_idling) {
     if (_strobeLastCycledOn == NULL || millis() - _strobeLastCycledOn > 400) {
       _strobeLastCycledOn = millis();
-      SetDimmerPower(_currentPower);
+      SetDimmerPower(HIGH_POWER);
     } else if (millis() - _strobeLastCycledOn > 200) {
       SetDimmerPower(IDLE_POWER);
     }
@@ -109,19 +121,17 @@ void VESCSmarterBrakes::ApplyStrobe() {
 }
 
 void VESCSmarterBrakes::DoLoop() {
-  int commReqDelayTime;
+  unsigned long commReqDelayTime = 0;
   float systemVoltage;
-  
+
   if (_loopStartMillis != NULL) {
     commReqDelayTime = 15-(millis()-_loopStartMillis);
   }
-  
+
   ReadMode();
   ApplyStrobe();
 
-  if (_mode == MODE_OFF) {
-    SetDimmerPower(OFF);
-  } else if (commReqDelayTime <= 0 && UART.getVescValues()) {
+  if (_mode != MODE_OFF && commReqDelayTime <= 0 && UART.getVescValues()) {
     _loopStartMillis = millis();
     systemVoltage = UART.data.inpVoltage;
 
@@ -159,7 +169,7 @@ void VESCSmarterBrakes::DoLoop() {
         }
       }
     }  
-  
+
     if (_brakeReleasingFrom != NULL && (_loopStartMillis - _brakeReleasingFrom > BRAKE_RELEASE_DEBOUNCE)) {
       // the brake release debounce period has passed, reset it
       _brakeReleasingFrom = NULL;
@@ -167,15 +177,15 @@ void VESCSmarterBrakes::DoLoop() {
       // honor the debounce by first making sure subsequent event didn't reactivate brake
       if (!_brakeActive) {
         // More than debounce ms have passed since ANY braking event, return to run power
-        SetDimmerPower(LOW_POWER);
+        ApplyMode();
       }
     }
     if (_idleSince == NULL) {
       if (_idling) {
         _idling = false;
-        SetDimmerPower(LOW_POWER);
+        ApplyMode();
       }
-    } else if (!_idling && (_mode == MODE_LOW || _mode == MODE_STEADY) && ((_loopStartMillis - _idleSince) > BRAKE_IDLE_CHILL_TIMER)) {
+    } else if (!_idling && ((_loopStartMillis - _idleSince) > BRAKE_IDLE_CHILL_TIMER)) {
       _idling = true;
       SetDimmerPower(IDLE_POWER);
     }
@@ -184,10 +194,12 @@ void VESCSmarterBrakes::DoLoop() {
         if (!_lightOff) {
           TransitionBrightness(LOW_POWER, OFF);
           _lightOff= true;
+          delay(5000);
         }
       } else if (_lightOff) {
         TurnOn();
       }
     }
   }
+  delay(1);
 }
--- VESCSmarterBrakes.h
+++ VESCSmarterBrakes.h
@@ -2,12 +2,11 @@
 #define VESCSmarterBrakes_h
 
 #include "Arduino.h"
-#include "VescUart.h"
 
 #define OFF 255
-#define IDLE_POWER 195
-#define LOW_POWER 115
-#define MEDIUM_POWER 80
+#define IDLE_POWER 230
+#define LOW_POWER 190
+#define MEDIUM_POWER 95
 #define HIGH_POWER 0
 #define BRAKE_RELEASE_DEBOUNCE 225
 #define BRAKE_IDLE_CHILL_TIMER 30000
@@ -16,41 +15,42 @@
 #define MODE_STEADY 1
 #define MODE_LOW 2
 #define MODE_OFF 3
+#define LAST_MODE 3
 #define BUTTON_PRESSED 0
 #define BUTTON_OFF 1
+#define MODE_EEPROM_ADDRESS 128
 
 class VESCSmarterBrakes
 {
   public:
     VESCSmarterBrakes(int dimmerPin, int buttonPin);
-		void TurnOn();
-		void DoLoop();
+    void TurnOn();
+    void DoLoop(int32_t RPM, float I, float V);
   private:
-		void ReadMode();
-		void ApplyStrobe();
-		void SetDimmerPower(int value);
-		void ChangeMode();
-		void TransitionBrightness(int dStart, int dStop);
+    void ReadMode();
+    void ApplyMode();
+    void ApplyStrobe();
+    void SetDimmerPower(int value);
+    void CycleMode();
+    void TransitionBrightness(int dStart, int dStop);
     int _dimmerPin;
     int _buttonPin;
-		int _startupSplashRate;
-		unsigned long _startupSplashDelay;
-		uint32_t _loopStartMillis;
-		int _mode;
-		int _currentPower;
-		int _lastButtonValue;
-		int _buttonValue;
-		int _runPower;
-		uint32_t _strobeLastCycledOn;
-		uint32_t _loopsInTarget;
-		uint32_t _idleSince;
-		uint32_t _brakeReleasingFrom;
-		uint32_t _lastDebounceTime;
-		VescUart UART;
-		bool _lightOff;
-		bool _idling;
-		bool _brakeActive;
-		bool _buttonUnpressed;
+    int _startupSplashRate;
+    unsigned long _startupSplashDelay;
+    unsigned long _loopStartMillis;
+    int _mode;
+    int _dimmerPower;
+    int _lastButtonValue;
+    int _buttonValue;
+    unsigned long _strobeLastCycledOn;
+    uint32_t _loopsInTarget;
+    unsigned long _idleSince;
+    unsigned long _brakeReleasingFrom;
+    unsigned long _lastDebounceTime;
+
+    bool _lightOff;
+    bool _idling;
+    bool _brakeActive;
 };
 
 #endif
