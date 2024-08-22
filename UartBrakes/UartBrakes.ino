71ddf182573ed513232df3cc468151fc4c35774d
Chris Becker <cbecker333@gmail.com>
2024-08-21 22:23:55 -0700

    set up compile switches for board support

--- UartBrakes/UartBrakes.ino
+++ UartBrakes/UartBrakes.ino
@@ -3,10 +3,24 @@
 // Pass in dimmer pin, mode switch pin
 VESCSmarterBrakes Brakes(3, 9);
 
+
 void setup() {
+  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega16U4__)
+
+  Serial1.begin(115200);
+  while (!Serial1) {;}
+  Brakes.SetSerial(&Serial1);
+
+  #endif
+
+  #if defined(__AVR_ATmega328P__)
+
   Serial.begin(115200);
   while (!Serial) {;}
   Brakes.SetSerial(&Serial);
+
+  #endif
+
   Brakes.TurnOn();
 }
 
