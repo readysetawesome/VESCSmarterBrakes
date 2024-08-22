4eed4329aa00e63562c3b59079493660c7e8978c
Chris Becker <cbecker333@gmail.com>
2024-08-21 18:31:02 -0700

    Create UartBrakes.ino

---
+++ src/UartBrakes.ino
@@ -0,0 +1,13 @@
+#include <VESCSmarterBrakes.h>
+
+// Pass in dimmer pin, mode switch pin
+VESCSmarterBrakes Brakes(3, 9);
+
+void setup() {
+  Brakes.TurnOn();
+}
+
+void loop() {
+  Brakes.DoLoop();
+  delay(10);
+}
