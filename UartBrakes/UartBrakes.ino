94f3eda7c50a96018e2e12350c0f76f8b2740241
Chris Becker <cbecker333@gmail.com>
2024-08-21 20:27:00 -0700

    set up serial in the sketch file

--- UartBrakes/UartBrakes.ino
+++ UartBrakes/UartBrakes.ino
@@ -4,6 +4,9 @@
 VESCSmarterBrakes Brakes(3, 9);
 
 void setup() {
+  Serial.begin(115200);
+  while (!Serial) {;}
+  Brakes.SetSerial(&Serial)
   Brakes.TurnOn();
 }
 
--- src/VESCSmarterBrakes.h
+++ src/VESCSmarterBrakes.h
@@ -10,7 +10,6 @@
 #define HIGH_POWER 0
 #define BRAKE_RELEASE_DEBOUNCE 225
 #define BRAKE_IDLE_CHILL_TIMER 30000
-#define DEFAULT_COMM_BAUD 250000
 #define MODE_STROBE 0
 #define MODE_STEADY 1
 #define MODE_LOW 2
@@ -26,6 +25,7 @@ class VESCSmarterBrakes
     VESCSmarterBrakes(int dimmerPin, int buttonPin);
     void TurnOn();
     void DoLoop();
+    void SetSerial(HardwareSerial* port);
   private:
     void ReadMode();
     void ApplyMode();
